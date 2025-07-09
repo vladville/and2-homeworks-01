b9b6915bf8c5a2e0ffc49b66be44f9edb5477693
Vladimir <vladville@gmail.com>
2025-07-09 12:56:14 +1000

    File storage added

---
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryFileImpl.kt
@@ -0,0 +1,97 @@
+package ru.netology.nmedia.repository
+
+import android.content.Context
+import androidx.core.content.edit
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import ru.netology.nmedia.dto.Post
+
+class PostRepositoryFileImpl(private val context: Context) : PostRepository {
+
+    private var index: Long = 1L
+    private var posts = emptyList<Post>()
+        set(value) {
+            field = value
+            sync()
+            data.value = posts
+        }
+    private var data = MutableLiveData(posts)
+
+    init {
+
+        val file = context.filesDir.resolve(FILE_NAME)
+
+        if (file.exists()) {
+            file.bufferedReader().use { reader ->
+                posts = gson.fromJson(reader, type)
+                index = (posts.maxOfOrNull { post -> post.id } ?: 0) + 1
+            }
+        } else {
+            sync()
+        }
+    }
+
+    override fun get(): LiveData<List<Post>> = data
+
+    override fun like(id: Long) {
+        posts = posts.map { post ->
+            if (post.id == id) {
+                if (post.likeByMe == false) {
+                    post.copy(likeByMe = true, likes = post.likes + 1)
+                } else {
+                    post.copy(likeByMe = false, likes = post.likes - 1)
+                }
+            } else {
+                post
+            }
+        }
+    }
+
+    override fun share(id: Long) {
+        posts = posts.map { post ->
+            if (post.id == id) {
+                post.copy(shares = post.shares + 1)
+            } else {
+                post
+            }
+        }
+    }
+
+    override fun removeById(id: Long) {
+        posts = posts.filter { it.id != id }
+    }
+
+    override fun save(post: Post) {
+        posts = if (post.id == 0L) {
+            listOf(
+                post.copy(
+                    id = index++,
+                    author = "Me",
+                    published = "now"
+                )
+            ) + posts
+        } else {
+            posts.map {
+                if (post.id == it.id) {
+                    it.copy(content = post.content)
+                } else it
+            }
+        }
+    }
+
+    private fun sync() {
+        context.openFileOutput(FILE_NAME, Context.MODE_PRIVATE).bufferedWriter().use { writer ->
+            writer.write(gson.toJson(posts, type))
+
+        }
+    }
+
+    companion object {
+        private const val FILE_NAME = "posts.json"
+        private val gson = Gson()
+        private val type = TypeToken.getParameterized(List::class.java, Post::class.java).type
+    }
+
+}
--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -4,13 +4,9 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import com.google.android.material.snackbar.Snackbar
-import ru.netology.nmedia.R
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
-import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
-import ru.netology.nmedia.repository.PostRepositorySharedPrefImpl
+import ru.netology.nmedia.repository.PostRepositoryFileImpl
 
 private val empty = Post(
     id = 0,
@@ -20,7 +16,7 @@ private val empty = Post(
 )
 
 class PostViewModel(application: Application) : AndroidViewModel(application) {
-    private val repository: PostRepository = PostRepositorySharedPrefImpl(application)
+    private val repository: PostRepository = PostRepositoryFileImpl(application)
     val data: LiveData<List<Post>> = repository.get()
     val edited = MutableLiveData(empty)
     fun like(id: Long) = repository.like(id)
