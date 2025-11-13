80aeef3356891fbdf8c5b48374764d6206eeedc2
Vladimir <vladville@gmail.com>
2025-11-13 15:11:59 +1000

    04_arch. Homework. Part 2

--- app/src/main/java/ru/netology/nmedia/api/ApiModule.kt
+++ app/src/main/java/ru/netology/nmedia/api/ApiModule.kt
@@ -17,7 +17,7 @@ import javax.inject.Singleton
 @Module
 class ApiModule {
     companion object {
-        private const val BASE_URL = "${BuildConfig.BASE_URL}/api/"
+        private const val BASE_URL = "${BuildConfig.BASE_URL}/api/slow/"
     }
 
     @Provides
